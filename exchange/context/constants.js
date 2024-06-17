f5c89f5b66a9f8e9e37e926a2b52a80592df89dd
ogweirdo <54372821+callmeweirdo@users.noreply.github.com>
2024-06-17 08:42:53 +0000

    ive made some key functions for the smart contract to target the provider and also the signer as well as writting scripts to connect to the meta mask wallet

--- exchange/context/constants.js
+++ exchange/context/constants.js
@@ -1,8 +1,9 @@
-import { Signer, ethers } from "ethers";
+import { ethers } from "ethers";
 import web3Modal from "web3modal";
 
 // INTERNAL IMPORT
 import ERC20ABI from "./abi.json";
+import { ChainId } from "@uniswap/sdk";
 
 export const ERC20_ABI = ERC20ABI;
 
@@ -13,23 +14,58 @@ export const V3_SWAP_ROUTER_ADDRESS =
 const TEST_ACCOUNT = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
 
 // FETCH CONTRACT
-const fetchTokenAddress = () => {
-    new ethers.Contract(ADDRESS, ERC20_ABI, signer);
+const fetchTokenContract = (signer, contract) => {
+    return new ethers.Contract(ADDRESS, ERC20_ABI, signer);
 }
+
+// web3 prvider function
 
 export const web3Provider = async () => {
     try{
         const web3Modal = new web3Modal();
         const connection = await web3Modal.connect();
-        const provider = ethers.providers.web3Provider(connection);
-    }catch (error){
+        const provider = new ethers.providers.web3Provider(connection);
+
+        // const network = await provider.getNetwork();
+
+        return provider;
+    }catch (error){  
         console.log(error);
     }
 }
 
+
+
 export const CONNECTING_CONTRACT = async (ADDRESS) => {
     try{
-         
+         const provider = await web3Provider();
+         const network = await provider.getNetwork();
+
+         const signer = provider.getSigner();
+         const contract = fetchTokenContract(signer, ADDRESS);
+
+        //  USER ADDRESS
+        // const userAddress = await signer.getAddress();
+        const balance = await contract.balance(TEST_ACCOUNT);
+        const name = await contract.name();
+        const supply = await contract.totalSupply();
+        const decimals = await contract.decimals();
+        const address = await contract.address();
+
+        const token =  {
+            address: address,
+            name: name,
+            Symbol: Symbol,
+            decimals: decimals,
+            supply: ethers.utils.formatEther(supply.toString()),
+            balance: ethers.utils.formatEther(balance.toString()),
+            ChainId:1,
+            // ChainId: network.ChainId
+        }
+
+        return token
+
+
     }catch(error){
         console.log(error);
     }
---
+++ exchange/context/context.js
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import { ethers } from "ethers";
+import { JSBI } from "jsbi";
+import Web3Modal from "web3modal";
+import toast from "react-hot-toast";
+
+//INTERNAL IMPORT Uniswap
+import { SwapRouter } from "@uniswap/universal-router-sdk";
+import { TradeType, Ether,Token, CurrencyAmount, Percent } from "@uniswap/sdk-core";
+import { Trade as V2Trade } from "@uniswap/v2-sdk";
+import { Pool, nearestUsableTick, TickMath, TICK_SPACINGS, FeeAmount, Trade as V3Trade, Route as RouteV3 } from "@uniswap/v3-sdk";
+
+import { MixedRouteTrade, Trade as RouterTrade  } from "@uniswap/router-sdk";
+import IUniswapV3Pool from "@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol";
+
+// INTERNAL IMPORT
+import {ERC20_ABI, web3Provider, CONNECTING_CONTRACT} from "./constants";
+import {shortAddress, parseErrorMsg} from "../utils/index";
+
+export const CONTEXT = React.createContext();
+
+export const PROVIDER = ({children}) => {
+    const TOKEN_SWAP = "TOKEN SWAP EXCHANGE";
+    const [loader, setLoader] = useState(fasle);
+    const [address, setAddress] = useState("");
+    const [chainId, setChainId] = useState("");
+
+    // NOTIFICATION
+    const notifyError = (msg) => toast.error(msg, {duration: 400});
+    const notifySuccess = (msg) => toast.success(msg, {duration: 400})
+
+    //CONNECT WALLET FUNCTION
+    const connect = async () => {
+        try{
+            if(!window.ethereum) return notifyError("Install Metamask");
+            const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            
+            if(accounts.length){
+                setAddress(accounts[0]);
+            }else{
+                notifyError("Sorry you haveno account");
+            }
+
+            const provider = await web3Provider();
+            const network = await provider.getNetwork();
+            setChainId(network.chainId);
+        }catch(error){
+            const errorMsg = parseErrorMsg(error);
+            notifyError(errorMsg);
+            console.log(errorMsg);
+        }
+    }
+
+}
